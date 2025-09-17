a7fa06af695e7536ec7cbacbf130c29cf868102d
Maximiliano Fairman <Maximiliano.fairman@stu.ocsb.ca>
2025-09-17 18:16:38 +0000

    areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,10 @@
+// copyright 2050
+// maximiliano fairman
+#include <cmath>
+#include <iostream>
+int main() {
+    std::cout << "if a rectangle has the dimensions:" << std ::endl;
+    std::cout << "6cm x 7cm" << std ::endl;
+    std::cout << "" << (7 * 2) << std ::endl;
+    std::cout << "Area is = " << (6 * 7) << "cm^2" << std ::endl;
+}
